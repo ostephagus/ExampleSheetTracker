be782abd9f681b2dcb3ac2c3b447fc5ddff16699
ostephagus <37499766+ostephagus@users.noreply.github.com>
2025-12-13 17:58:16 +0000

    Added timer field for questions

--- DataManager/Question.cs
+++ DataManager/Question.cs
@@ -27,23 +27,28 @@ namespace DataManager
         private bool starred;
         private Progress progress;
         private TriState doing;
+        private TimeSpan timeTaken;
 
         [XmlAttribute]
         public int Number { get => number; set => number = value; }
         public bool Starred { get => starred; set => starred = value; }
         public Progress Progress { get => progress; set => progress = value; }
         public TriState Doing { get => doing; set => doing = value; }
+        public TimeSpan TimeTaken { get => timeTaken; set => timeTaken = value; }
 
         [XmlIgnore]
         public override string Name { get => $"Q{number}"; }
 
-        public Question(int number, bool starred, Progress progress, TriState doing)
+        public Question(int number, bool starred, Progress progress, TriState doing, TimeSpan timeTaken)
         {
             this.number = number;
             this.starred = starred;
             this.progress = progress;
             this.doing = doing;
+            this.timeTaken = timeTaken;
         }
+
+        public Question(int number, bool starred, Progress progress, TriState doing) : this(number, starred, progress, doing, TimeSpan.Zero) { }
 
         public Question(int number) : this(number, false, Progress.NotStarted, TriState.Yes) { }
 
